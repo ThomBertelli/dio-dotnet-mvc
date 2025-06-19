f4ad55293de3c33d0f105a4e0b4de3bc888f2105
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2025-06-18 23:13:33 -0300

    feat: initialize MVC project structure

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -39,5 +39,68 @@ namespace MVC.Controllers
             }
             return View(contato);
         }
+
+        public IActionResult Editar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            return View(contato);
+        }
+
+        [HttpPatch, ActionName("Editar")]
+        public IActionResult Editar(int id, Contato contato)
+        {
+            if (id != contato.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Contatos.Update(contato);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+
+        public IActionResult Excluir(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            return View(contato);
+        }
+
+        [HttpPost, ActionName("Excluir")]
+        public IActionResult ExcluirConfirmado(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contatos.Remove(contato);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Detalhes(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            return View(contato);
+        }
+        
+        
     }
 }
